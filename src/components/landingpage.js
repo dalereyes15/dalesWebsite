9527f9f520aaf94925b7f5c0110462405914e560
Dale Reyes <dalereyes4@gmail.com>
2020-05-31 19:54:48 -0500

    Dale Reyes: Changing background to graduation image

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -5,7 +5,7 @@ class LandingPage extends Component{
     render(){
         return(
             <div style = {{width: '100%', margin: 'auto'}}>
-                <Grid className="landing-grid">
+                <Grid className="landing-grid" style={{background: 'url(https://lh3.googleusercontent.com/pw/ACtC-3faM2z12Q-Es3xTwYeUjDyw9nXJxf3x0SOv9YyOImLoxMXt9mIJeI7_DTICPSxPxGNokh0zKp5BUl1RXswvJK3ct2EtHOOFJ1j1TL8S9AjVY1nsSuY0mjsoy7AbK10Auw5RLLcvUIu6dYewTDL-hbq84w=w1191-h794-no?authuser=0) center / cover'}}>
                     <Cell col={12}>
                         <img
                             src = "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/40773610_10210245282247026_1493580391922008064_n.jpg?_nc_cat=108&_nc_sid=09cbfe&_nc_oc=AQkRt0cKQtL8ZIwFVwa7Qsxd-ntlVx-EyPB8BZZdg6vFm03o9Fb1sKH4yABoez_mm9qHEPgXm46ZumBFqHRZ7E27&_nc_ht=scontent-ort2-1.xx&oh=410fa327739dfa9ceef7322e99f56049&oe=5EFAC8A2"
