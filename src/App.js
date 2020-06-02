c3016729d23c350502dfd2bea7622ab926f68607
Dale Reyes <dalereyes4@gmail.com>
2020-06-02 17:47:54 -0500

    Dale Reyes: project tabs working

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {Link} from 'react-router-dom';
 function App() {
   return (
       <div className="demo-big-content">
-          <Layout style={{background: 'url(https://lh3.googleusercontent.com/pw/ACtC-3faM2z12Q-Es3xTwYeUjDyw9nXJxf3x0SOv9YyOImLoxMXt9mIJeI7_DTICPSxPxGNokh0zKp5BUl1RXswvJK3ct2EtHOOFJ1j1TL8S9AjVY1nsSuY0mjsoy7AbK10Auw5RLLcvUIu6dYewTDL-hbq84w=w1191-h794-no?authuser=0) center / cover'}}>
+          <Layout>
               <Header className="header-color" title="Dale Reyes" scroll>
                   <Navigation>
                       <Link to="/resume">Resume</Link>
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,51 @@
 import React, {Component} from 'react';
+import {Tab, Tabs} from 'react-mdl';
 
 class Projects extends Component{
+    constructor(props){
+        super(props );
+        this.state = {activeTab: 0};
+    }
+
+    toggleCategories(){
+        if (this.state.activeTab === 0){
+            return(
+                <div><h1>This is React</h1></div>
+            );
+        }
+        else if(this.state.activeTab === 1) {
+            return(
+                <div><h1>This is Java</h1></div>
+            );
+        }
+        else if(this.state.activeTab === 2) {
+            return(
+                <div><h1>This is Android Studio</h1></div>
+            );
+        }
+        else if(this.state.activeTab === 3) {
+            return(
+                <div><h1>This is YouTube</h1></div>
+            );
+        }
+
+    }
+
+
+
     render(){
         return(
-            <div>
-                <h1>Projects Page</h1>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
+                    <Tab>React</Tab>
+                    <Tab>Java</Tab>
+                    <Tab>Android Development</Tab>
+                    <Tab>YouTube</Tab>
+                </Tabs>
+
+                <section clasName = "projects-grid">
+                    {this.toggleCategories()}
+                </section>
             </div>
         )
     }
